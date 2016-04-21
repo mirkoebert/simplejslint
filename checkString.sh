8b89e379cb91cf7e4687dd1fb58c49e339de8db2
mirkoebert <mirko.ebert@gmail.com>
2016-04-21 13:10:28 +0200

    analyse single files to

--- checkString.sh
+++ checkString.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-set -x  
+analyzeOneFile() {
+    c=`fgrep -o ${1} ${2} | wc -l`
+    d=`date`
+    echo "$d, \"Count ${1}\", $c, $2" | tee -a log
+}
+
+#set -x  
 dir=$1
 str=$2
 if [ -d "$dir" ]; then
-    d=`date`
+    #d=`date`
     for filename in ${dir}*.js; do
-        c=`fgrep -o ${str} ${filename} | wc -l`
-        echo "$d, \"Count ${str}\", $c, $filename" | tee -a log
+        analyzeOneFile ${str} ${filename}
+        #c=`fgrep -o ${str} ${filename} | wc -l`
+        #echo "$d, \"Count ${str}\", $c, $filename" | tee -a log
     done
 else
-    echo "No dir"
+    analyzeOneFile ${str} ${dir}
+    #echo "No dir"
 fi
 
