58b68be7370e1b74e41bfa5d91c9e1d9815bdd40
mirkoebert <mirko.ebert@gmail.com>
2016-04-27 17:29:12 +0200

    fix script name

--- simplejspreprocessor.sh
+++ simplejspreprocessor.sh
@@ -5,46 +5,19 @@ f=tmp
 version=$2
 
 
-count=`grep -n "//#ifdef\s\s*$version" $f | wc -l`
-echo "Incude count: $count"
+label=`grep -n "//#ifdef\s\s*\w" $f | awk '{print $2}' | sort | uniq`
 
-withoutinclude=t.js
-
-#for i in {1..$count}
-while [ $count -gt 0 ]
+for l in $label 
 do
-    echo "Loop $count"
-    a=`grep -n "//#ifdef\s\s*$version" $f | head -n 1 |awk -F: '{print $1}'`
-    a=$((a-1))
-    #echo "AAA: $a"
-    t=`cat $f | wc -l`
-    z=`grep -n "//#endif" $f | head -n 1 | awk -F: '{print $1}'`
-    echo "Preprocessor from line $a to line $z, total lines: $t"
-    x=$((t-z))
-
-    head -n $a $f >  $withoutinclude
-    tail -n $x $f >> $withoutinclude
-    cp $withoutinclude $f
-
-    count=$(( $count - 1 ))
+    #echo $l
+    if [ "$l" == "$version" ] 
+    then
+        echo "Keep $l"
+    else
+        echo "Delete $l"
+        ./simplejspreprocessor_cut.sh $f $l
+        mv t.js $f
+    fi
 done
 
-
-#a=`grep -n "//#ifdef $version" $f | awk -F: '{print $1}'`
-
-#if [[ -n "$a" ]]; then
-#    z=`grep -n "//#endif" $f | awk -F: '{print $1}'`
-#    t=`cat $1 | wc -l`
-#    echo "Preprocessor from $a to $z Total lines: $t"
-
-
-#    x=$((t-z))
-#    withinclude=${f}_wi.js
-#    withoutinclude=${f}_wo.js
-
-#   head -n $a $f >  $withoutinclude
-#   tail -n $x $f >> $withoutinclude
-#else
-#    echo "Warning not precompier tag found: $2"
-#fi
 
