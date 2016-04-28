a93490017e9e829061bb597c19cfe974a2a849bb
mirkoebert <mirko.ebert@gmail.com>
2016-04-28 13:32:36 +0200

    add new JS speed rule

--- src/checkStringAll.sh
+++ src/checkStringAll.sh
@@ -2,10 +2,11 @@
 
 dir=$1
 src/checkString.sh $dir eval "Potential Slow and Code Smell"
-src/checkString.sh $dir '$(' "JQuery Function"
-src/checkString.sh $dir '$.' "JQuery Utility Function"
+src/checkString.sh $dir '$(' "JQuery Function, Potential Slow"
+src/checkString.sh $dir '$.' "JQuery Utility Function, Potential Slow"
 src/checkString.sh $dir with "Potential Slow"
 src/checkString.sh $dir new  "Potential Slow"
+src/checkString.sh $dir "document.write"  "Potential Slow"
 
 src/checkRegex.sh  $dir "for\s+in" "Potential Slow"
 src/checkRegex.sh  $dir "return\s+null" "Code Smell"
