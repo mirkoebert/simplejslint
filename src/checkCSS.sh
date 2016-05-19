000ed1c764a54184a831b44033ae65e69c10e2f7
Uwe Bessle <Uwe.Bessle@iteratec.de>
2016-05-19 18:10:06 +0200

    performance improvement in checkCSS : do csslint only once and extract error and warnings number from the temporar csslint result file

--- src/checkCSS.sh
+++ src/checkCSS.sh
@@ -15,8 +15,9 @@ outputFile=$2
 
 analyzeOneFile() {
 	inputFile=$1
-	warnings=`csslint --quiet --ignore=box-sizing,adjoining-classes,compatible-vendor-prefixes,gradients,text-indent --format=compact  $inputFile | grep "Warning -" | wc -l`
-	errors=`csslint --quiet --ignore=box-sizing,adjoining-classes,compatible-vendor-prefixes,gradients,text-indent --format=compact  $inputFile | grep "Error -" | wc -l`
+	csslint --quiet --ignore=box-sizing,adjoining-classes,compatible-vendor-prefixes,gradients,text-indent --format=compact  $inputFile > .csslint.tmp
+	warnings=`cat .csslint.tmp | grep "Warning -" | wc -l`
+	errors=`cat .csslint.tmp | grep "Error -" | wc -l`
 	d=`date`
 	echo "$d, $inputFile, $warnings, \"CSS Warnings Count\"" | tee -a $outputFile
 	echo "$d, $inputFile, $errors, \"CSS Errors Count\"" | tee -a $outputFile
