542445a4e7524e4f5ab059aaf84684a5fe8505f8
Uwe Bessle <Uwe.Bessle@iteratec.de>
2016-06-16 11:54:52 +0200

    fix regular expressions to measure media query sizes

--- src/checkAllRules.sh
+++ src/checkAllRules.sh
@@ -31,12 +31,12 @@ fi
 if [[ $inputFile == *.css ]]; then
 	src/checkCssLint.sh $inputFile $outputFile
 	src/checkString.sh $inputFile mediaQueryCount '@media' "Media Query" $outputFile
-	src/checkRegex.sh $inputFile breakpointMCount '@media\s+\(min-width:\s*28em\)' "Breakpoint M" $outputFile
-	src/checkRegex.sh $inputFile breakpointLCount '@media\s+\(min-width:\s*48em\)' "Breakpoint L" $outputFile
-	src/checkRegex.sh $inputFile breakpointXLCount '@media\s+\(min-width:\s*62em\)' "Breakpoint XL" $outputFile
-	src/countMultiLineRegexBytes.sh $inputFile breakpointMBytes '@media\s+\(min-width:\s*28em\)\s*{([^{}]*{[^{}]*})*[^{}]*}' "Breakpoint M bytes" $outputFile
-	src/countMultiLineRegexBytes.sh $inputFile breakpointLBytes '@media\s+\(min-width:\s*48em\)[^}]*}' "Breakpoint L bytes" $outputFile
-	src/countMultiLineRegexBytes.sh $inputFile breakpointXLBytes '@media\s+\(min-width:\s*62em\)[^}]*}' "Breakpoint XL bytes" $outputFile
+	src/checkRegex.sh $inputFile breakpointMCount '@media\s+[^{}]*\(min-width:\s*28em\)' "Breakpoint M" $outputFile
+	src/checkRegex.sh $inputFile breakpointLCount '@media\s+[^{}]*\(min-width:\s*48em\)' "Breakpoint L" $outputFile
+	src/checkRegex.sh $inputFile breakpointXLCount '@media\s+[^{}]*\(min-width:\s*62em\)' "Breakpoint XL" $outputFile
+	src/countMultiLineRegexBytes.sh $inputFile breakpointMBytes '@media\s+[^{}]*\(min-width:\s*28em\)[^{}]*{([^{}]*{[^{}]*})*[^{}]*}' "Breakpoint M bytes" $outputFile
+	src/countMultiLineRegexBytes.sh $inputFile breakpointLBytes '@media\s+[^{}]*\(min-width:\s*48em\)[^{}]*{([^{}]*{[^{}]*})*[^{}]*}' "Breakpoint L bytes" $outputFile
+	src/countMultiLineRegexBytes.sh $inputFile breakpointXLBytes '@media\s+[^{}]*\(min-width:\s*62em\)[^{}]*{([^{}]*{[^{}]*})*[^{}]*}' "Breakpoint XL bytes" $outputFile
 	src/countMultiLineRegexBytes.sh $inputFile mediaQueryBytes '@media\s+[^{}]*{([^{}]*{[^{}]*})*[^{}]*}' "Media Query bytes" $outputFile
 fi
 
